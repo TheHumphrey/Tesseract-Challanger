58e98723cf4a06be2cbfc6480d6ea7a5b28cced9
Fernando <47810697+TheHumphrey@users.noreply.github.com>
2020-02-06 08:54:06 -0300

    Implementando a barra de busca e sua logica
    Foi implementado a Barra de busca(Filtro de usuários pelo login) e sua logica.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import api from "./services/api";
 
 //Importando os Componentes da aplicação
 import DevItem from "./components/DevItem/index";
+import SearchDev from "./components/SearchDev/index";
 
 export default function App() {
   //Estados da aplicação
   const [devs, setDevs] = useState([]);
+  const [filter, setFilter] = useState([]);
 
   //o bloco Abaixo esta realizando a consulta na API e armazenando os dados no estado
   useEffect(() => {
@@ -24,13 +26,21 @@ export default function App() {
   //O bloco abaixo esta retornando o que sera apresentado para o usuário em tela
   return (
     <div className="App">
-      <aside />
+      <aside>
+        <SearchDev callback={setFilter} />
+      </aside>
       <main>
         <div className="List">
           <ul>
-            {devs.map(dev => (
-              <DevItem key={dev.id} dev={dev} />
-            ))}
+            {devs
+              .filter(dev =>
+                dev.login
+                  .toLowerCase()
+                  .includes(filter.toString().toLowerCase())
+              )
+              .map(dev => (
+                <DevItem key={dev.id} dev={dev} />
+              ))}
           </ul>
         </div>
       </main>
---
+++ src/components/SearchDev/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Search(props) {
+  return (
+    <div>
+      <input type="text" onChange={e => props.callback(e.target.value)} />
+    </div>
+  );
+}
+
+export default Search;
